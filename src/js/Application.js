fe22464fdf4b4f3c4fa4a39f94b18238b8bef151
Ivan <ivan@spasov.xyz>
2023-01-22 20:40:03 +0200

    add progress bar and planet boxes

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import { createHtmlTagObject } from "html-webpack-plugin";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,11 +11,61 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
+    this.planets = "";
 
-    this.emit(Application.events.READY);
+    this._loading = document.querySelector('.progress');
+
+    this._load().then(() => this.emit(Application.events.READY));
   }
-}
+
+  async _load() {
+    let URL = 'https://swapi.boom.dev/api/planets/';
+
+    this._startLoading();
+
+    while (URL) {
+      const response = await fetch(URL);
+
+      const planetCollection = await response.json();
+      URL = planetCollection.URL;
+
+      this.planets = [...this.planets, ...planetCollection.results];
+    }
+
+    this._create();
+
+    this._stopLoading();
+  }
+
+  _create() {
+    this.planets.forEach(planet => {
+      const box = document.createElement('div');
+      box.classList.add('box');
+
+      box.innerHTML = this._render({
+        name: planet.name,
+      });
+
+      document.body.querySelector(".main").appendChild(box);
+    })
+  }
+
+  _startLoading() {
+    this._loading.style.display = 'block';
+  }
+
+  _stopLoading() {
+    this._loading.style.display = 'none';
+  }
+
+  _render({name}) {
+    return `
+  <div>
+    <h4 class="mb-3">${name}</h4>
+    <figure class="image is-64x64">
+      <img src="${image}">
+    </figure>
+  </div>
+  `;
+  }
+};
